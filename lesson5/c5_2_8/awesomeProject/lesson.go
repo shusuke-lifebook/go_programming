e8f59e4e504f87c49da5ff09d9f0e9714396dce5
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-09-20 20:46:48 +0900

    5-2チャネルでゴルーチンと値のやりとりをしようの学習完了

---
+++ lesson5/c5_2_8/awesomeProject/lesson.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 2)
+	ch <- 100
+	fmt.Println(len(ch))
+	ch <- 200
+	fmt.Println(len(ch))
+
+	x := <-ch
+	fmt.Println(x)
+	fmt.Println(len(ch))
+
+	ch <- 300
+	fmt.Println(len(ch))
+}
