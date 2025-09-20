5b15d56cc68a2cebbeeaec9005eb084b0f0b26e6
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-09-20 16:26:25 +0900

    5-1 並行処理を作ろうの学習完了

---
+++ lesson5/c5_1_8/awesomeProject/lesson.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func normal(s string) {
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func goroutine(s string, wg *sync.WaitGroup) {
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+	// wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go goroutine("world", &wg)
+	normal("hello")
+	// time.Sleep(2000 * time.Microsecond)
+	wg.Wait()
+}
