75e0ff0b8045ec0bce51ad2cf7c3812ae81a359c
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-09-27 16:42:00 +0900

    Lesson5の学習完了

---
+++ lesson5/c5_7_1/awesoemProject/lesson.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(map[string]int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			c["key"] += 1
+		}
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			c["key"] += 1
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	fmt.Println(c, c["key"])
+}
