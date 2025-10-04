d0f879929505ccb456fd6eae1eaa9dd0e0780c53
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-10-04 17:28:18 +0900

    7-2-1 json.Unmarshal 関数で JSON を構造体に変換しようの学習完了

---
+++ lesson7/c7_2_1/awesomeProject/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name      string
+	Age       int
+	Nicknames []string
+}
+
+func main() {
+	b := []byte(`{"name":"mike","age": 20,"nicknames":["a", "b", "c"]}`)
+	var p Person
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(p.Name, p.Age, p.Nicknames)
+}
