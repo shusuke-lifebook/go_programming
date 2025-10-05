b21d16f69cff0f74a3f78702de7e5ecc972e049e
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-10-05 21:02:24 +0900

    7-2-5 json.Unmarshal 関数をカスタマイズしようまで学習完了

---
+++ lesson7/c7_2_18/awesomeProject/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type T struct {
+}
+
+type Person struct {
+	Name      string   `json:"name,omitempty"`
+	Age       int      `json:"age,omitempty"`
+	Nicknames []string `json:"nicknames,omitempty"`
+	T         *T       `json:"T,omitempty"`
+}
+
+func (p Person) MarshalJSON() ([]byte, error) {
+	v, err := json.Marshal(&struct {
+		Name string
+	}{
+		Name: "Mr." + p.Name,
+	})
+	return v, err
+}
+
+func main() {
+	b := []byte(`{"name":"Mike","age":20,"nicknames":[]}`)
+	var p Person
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(p.Name, p.Age, p.Nicknames)
+
+	v, _ := json.Marshal(p)
+	fmt.Println(string(v))
+}
