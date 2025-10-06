bd62bae35989292558d249a68fbe9b3e0578843a
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-10-06 17:15:31 +0900

    7-3-3 SQL インジェクションの例を確認しようの学習完了

---
+++ lesson7/c7_3_11/awesomeProject/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DbConnection *sql.DB
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	DbConnection, _ := sql.Open("sqlite3", "./example.sql")
+	defer DbConnection.Close()
+
+	cmd := `CREATE TABLE IF NOT EXISTS person(
+		name STRING,
+		age INT
+	)`
+	_, err := DbConnection.Exec(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tableName := "person"
+	cmd = fmt.Sprintf("SELECT * FROM %s", tableName)
+	rows, _ := DbConnection.Query(cmd)
+	defer rows.Close()
+
+	var pp []Person
+	for rows.Next() {
+		var p Person
+		err := rows.Scan(&p.Name, &p.Age)
+		if err != nil {
+			log.Println(err)
+		}
+		pp = append(pp, p)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, p := range pp {
+		fmt.Println(p.Name, p.Age)
+	}
+}
