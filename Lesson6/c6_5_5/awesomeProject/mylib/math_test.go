687a6a0213782046cf2d5df41b1a97b2389e9b38
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-04-05 13:53:56 +0900

    パッケージ全体のコード例を追加

---
+++ Lesson6/c6_5_5/awesomeProject/mylib/math_test.go
@@ -0,0 +1,23 @@
+package mylib
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAverage(t *testing.T) {
+	v := Average([]int{1, 2, 3, 4, 5})
+	if v != 3 {
+		t.Error("Expected 3, got", v)
+	}
+}
+
+func ExampleAverage() {
+	v := Average([]int{1, 2, 3, 4, 5})
+	fmt.Println(v)
+}
+
+func Example() {
+	v := Average([]int{1, 2, 3, 4, 5, 6})
+	fmt.Println(v)
+}
